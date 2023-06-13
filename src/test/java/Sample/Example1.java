1ad37fd50cf6691c5ce4413561889fb3ec252241
Rupendra <Rupendra@LAPTOP-IUJNIELA>
2023-06-13 12:46:54 +0530

    changes done by sirisha

--- src/test/java/Sample/Example1.java
+++ src/test/java/Sample/Example1.java
@@ -14,6 +14,7 @@ public class Example1 {
 	@Test(groups="regression")
 	public void exa() {
 		System.out.println("Example1 class of exa method excuted");
+		System.out.println("branch created");
 	}
 
 }
